45b0ed1489cccdfe89663711656d11579c17b996
khalifahnur1095 <khalifahnur1095@gmail.com>
2025-02-11 20:49:21 +0300

    added:router this router => signup,signin,fetchinfo,logout which are for the user

--- routes/admin/adminrouter.ts
+++ routes/admin/adminrouter.ts
@@ -2,8 +2,11 @@ import express from 'express';
 
 const router = express.Router();
 
-const AdminSignup = require('../../controllers/auth/admin/signup')
-const AdminSignin = require('../../controllers/auth/admin/signin')
+const AdminSignup = require('../../controllers/auth/admin/signup');
+const AdminSignin = require('../../controllers/auth/admin/signin');
+const FetchInfo = require("../../controllers/auth/admin/fetchinfo");
+const authenticateUser = require("../../middleware/middleware");
+const LogoutAdmin = require("../../controllers/auth/admin/logout");
 
 // const AuthController = require("../controllers/AuthController");
 // const forgotPsswdController = require("../controllers/ForgotPsswdController");
@@ -16,5 +19,7 @@ router.post("/SignIn", AdminSignin);
 // router.post("/forgot-password",forgotPsswdController.forgotPsswd);
 // router.post("/verify-code",verifyCodeController.verifyCode);
 // router.post("/reset-password",resetPasswordController.resetPassword);
+router.get("/fetchinfo", authenticateUser,FetchInfo);
+router.post("/logout",authenticateUser,LogoutAdmin);
 
 module.exports = router;
