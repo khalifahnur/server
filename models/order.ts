3b6f7ce688317932019c8220ad6969adfb1ad719
khalifahnur1095 <khalifahnur1095@gmail.com>
2025-02-11 21:06:29 +0300

    added schema for orders

---
+++ models/order.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const MenuItemSchema = new Schema({
+  id: { type: String, required: true },
+  image: { type: String, required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  cost: { type: Number, required: true },
+  quantity: { type: Number, required: true, default: 0 },
+});
+
+const OrderSchema = new Schema({
+  userId: { type: String, required: true },
+  restaurantId: { type: String, required: true },
+  reservationId: { type: String, required: true },
+  tableNumber: { type: String, required: true },
+  diningArea: { type: String, required: true },
+  menu: { type: [MenuItemSchema], required: true },
+  status: {
+    type: String,
+    required: true,
+    enum: ["Served", "Not-served"],
+    default: "Not-served",
+  },
+  paid: {
+    type: String,
+    required: true,
+    enum: ["Paid", "Unpaid"],
+    default: "Unpaid",
+  },
+  takenBy: { type: String, default: "" },
+  createdAt: { type: Date, default: Date.now },
+});
+
+OrderSchema.index({ userId: 1, restaurantId: 1, tableNumber: 1 });
+
+const Order = model("Order", OrderSchema);
+module.exports = Order;
