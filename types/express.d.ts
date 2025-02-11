75fa3aae7b394be55f7a0bbd99da39ff277dbdc1
khalifahnur1095 <khalifahnur1095@gmail.com>
2025-02-11 21:07:59 +0300

    docker config for kafka and mongodb

--- types/express.d.ts
+++ types/express.d.ts
@@ -1,10 +1,11 @@
 // types/express.d.ts
-import { JwtPayload } from 'jsonwebtoken';
+import { JwtPayload } from "jsonwebtoken";
 
 declare global {
   namespace Express {
     interface Request {
       user?: { id: string } & JwtPayload;
+      restaurantId?: { id: string } & JwtPayload;
     }
   }
 }
