5136508740f22b0d24c6fca7a7305169f58dd557
khalifahnur1095 <khalifahnur1095@gmail.com>
2025-02-11 20:52:50 +0300

    controller that deletes cookie after logout

---
+++ controllers/auth/admin/logout.ts
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
+
+import GenerateSecretKey from "../../../lib/GenerateSecretKey";
+
+interface AuthenticatedRequest extends Request {
+  user?: {
+    id: string;
+  };
+}
+
+const secretKey = process.env.JWT_SECRET_KEY || GenerateSecretKey();
+const LogoutAdmin = async (req: AuthenticatedRequest, res: Response) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    return res.status(200).json({ message: "Logged out successfully" });
+  } catch (error: any) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "Token has expired" });
+    }
+
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
+
+module.exports = LogoutAdmin;
