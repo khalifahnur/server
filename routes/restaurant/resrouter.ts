12efba2cb687eebef80dd3c1af224bfe04062916
khalifahnur1095 <khalifahnur1095@gmail.com>
2025-02-11 20:50:28 +0300

    added :/fetch-all-restaurants

--- routes/restaurant/resrouter.ts
+++ routes/restaurant/resrouter.ts
@@ -1,15 +1,28 @@
+import upload from "../../middleware/upload";
+
 const express = require("express");
 const router = express.Router();
-const AddContoller = require('../../controllers/restaurant/addrestaurant');
-const AddMenuController  = require("../../controllers/restaurant/addmenu");
+
+const AddContoller = require("../../controllers/restaurant/addrestaurant");
+const AddMenuController = require("../../controllers/restaurant/addmenu");
 const RemoveController = require("../../controllers/restaurant/removemenu");
-const UpdateController = require("../../controllers/restaurant/modifyitemcost");
-const authenticateUser = require('../../middleware/middleware')
+const authenticateUser = require("../../middleware/middleware");
+const attachRestaurantId = require("../../middleware/attachRestaurantId");
+const UpdateController = require("../../controllers/restaurant/editmenu");
+const getMenu = require("../../controllers/restaurant/getmenurestaurant");
 
-router.post("/addrestaurant", authenticateUser, AddContoller);
-router.post("/:id/addmenu/:menuType", AddMenuController);
-router.delete("/:id/deletemenu/:menuType/:itemId",RemoveController);
-router.put("/:id/updatemenu/:menuType/:itemId",UpdateController);
+const FetchAllRestaurants = require("../../controllers/restaurant/fetchrestaurants");
+const FetchResNearMe = require("../../controllers/restaurant/fetchresnearme");
 
+
+router.post("/addrestaurant", authenticateUser,upload.single("image"), AddContoller);
+
+router.post("/addmenu/:menuType", attachRestaurantId, upload.single("image"), AddMenuController);
+router.delete("/deletemenu/:menuType/:itemId", attachRestaurantId, RemoveController);
+router.put("/updatemenu/:menuType/:itemId", attachRestaurantId, UpdateController);
+router.get("/menu",attachRestaurantId,getMenu);
+
+router.get("/fetch-all-restaurants",FetchAllRestaurants);
+router.post("/fetch-restaurants-near-me",FetchResNearMe);
 
 module.exports = router;
