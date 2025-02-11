10c5f913acacd0ad24253f790e46827e2e093d4f
khalifahnur1095 <khalifahnur1095@gmail.com>
2025-02-11 20:45:06 +0300

    added: payment global router and also updatedcors

--- index.ts
+++ index.ts
@@ -2,49 +2,101 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-//routers
-const restaurantrouter = require("./routes/restaurant/resrouter");
-const adminauthrouter = require("./routes/admin/adminrouter");
+import "./lib/SecretKeyConfig";
+import "./lib/SecretKeyConfigUser";
+
+const startReservationCronJob = require("./controllers/reservation/reservationupdates");
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3002;
-const MongodbConn = process.env.MONGODB_CONN || '';
+const PORT = process.env.PORT || 3003;
+const MongodbConn = process.env.MONGODB_CONN || "";
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const corsOptions = {
+  origin: ["http://localhost:3000", "http://192.168.100.197:3002"],
+  // origin:'*',
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
-//mongo db connection
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "5mb" }));
+app.use(bodyParser.json({ limit: "5mb" }));
 
 mongoose
-  .connect(
-    MongodbConn
-  )
+  .connect(MongodbConn)
   .then(() => {
+    startReservationCronJob();
     console.log("MongoDB successfully connected");
   })
   .catch((error) => {
     console.log("MongoDB connection Error", error);
   });
 
+const restaurantrouter = require("./routes/restaurant/resrouter");
+const adminauthrouter = require("./routes/admin/adminrouter");
+const userauthrouter = require("./routes/user/userrouter");
+const reservationrouter = require("./routes/reservation/reservationrouter");
+const tablerouter = require("./routes/table/tablerouter");
+const paymentrouter = require("./routes/payment/paymentrouter");
+
 /**
  * /swiftab/restaurant/addrestaurant/
- * /swiftab/restaurant/:id/addmenu/:menuType
- * /swiftab/restaurant/:id/deletemenu/:menuType/:itemId
- * /swiftab/restaurant/:id/updatemenu/:menuType/:itemId
+ * /swiftab/restaurant/addmenu/:menuType
+ * /swiftab/restaurant/deletemenu/:menuType/:itemId
+ * /swiftab/restaurant/updatemenu/:menuType/:itemId
+ * /swiftab/restaurant/menu
+ * /swiftab/restaurant/fetch-restaurants-near-me
+ * /swiftab/restaurant/fetch-restaurant-table
+ *
  */
 app.use("/swiftab/restaurant", restaurantrouter);
 
 /**
- * /swiftab/auth/SignUp
- * /swiftab/auth/SignIn
+ * /swiftab/auth/admin/SignUp
+ * /swiftab/auth/admin/SignIn
+ * /swiftab/auth/admin/fetchinfo
+ * /swiftab/auth/admin/logout
  */
-app.use("/swiftab/auth", adminauthrouter)
+app.use("/swiftab/auth/admin", adminauthrouter);
 
-//event listener of connection
+/**
+ * /swiftab/auth/user/SignUp
+ * /swiftab/auth/user/SignIn
+ */
+app.use("/swiftab/auth/user", userauthrouter);
 
+/**
+ * /swiftab/reservation/:userId/reserve/:restaurantId
+ * /swiftab/reservation/:restaurantId
+ * /swiftab/reservation/:userId/active |cancelled | completed
+ * /swiftab/reservation/fetched-active
+ */
+app.use("/swiftab/reservation", reservationrouter);
+
+/**
+ * /swiftab/table/save-tables
+ * /swiftab/table/fetch-tables
+ * /swiftab/table/fetch-restaurant-info
+ * /swiftab/table/save-layout-info
+ * /swiftab/table/fetch-restaurant-tables
+ * /swiftab/table/fetch-restaurant-table/:restaurantId
+ */
+app.use("/swiftab/table", tablerouter);
+
+/**
+ * /swiftab/payment/initiate-payment
+ * /swiftab/payment/transfer-payment
+ */
+app.use("/swiftab/payment", paymentrouter);
+
+// Event listener for connection
 app
   .listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
