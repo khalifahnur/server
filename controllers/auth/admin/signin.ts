061dc1e8f1291a014014349c6cc081fc8bdaa80b
khalifahnur <khalifahnur1095@gmail.com>
2025-03-04 14:35:06 +0300

    fix:cookie for different site

--- controllers/auth/admin/signin.ts
+++ controllers/auth/admin/signin.ts
@@ -31,6 +31,7 @@ const loginAdmin = async (req: Request, res: Response) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
+      sameSite:'none',
       maxAge: 24 * 60 * 60 * 1000,
     });
 
