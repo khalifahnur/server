d6fe406c7bfc26902ec38140e048aafbe0195666
khalifahnur <khalifahnur1095@gmail.com>
2025-09-06 09:27:13 +0300

    ref:waiter sign up

--- routes/waiter/waiterrouter.ts
+++ routes/waiter/waiterrouter.ts
@@ -5,8 +5,9 @@ const router = express.Router();
 const waiterSignUp = require("../../controllers/auth/waiter/signup");
 const waiterSignIn = require("../../controllers/auth/waiter/signin");
 const waiterPassword = require("../../controllers/auth/waiter/waiterPassword");
+const attachRestaurantId = require("../../middleware/attachRestaurantId");
 
-router.post('/waiter-app-signup',waiterSignUp);
+router.post('/waiter-app-signup',attachRestaurantId, waiterSignUp);
 router.post('/waiter-app-signin', waiterSignIn);
 router.post('/waiter-new-password',waiterPassword);
 
